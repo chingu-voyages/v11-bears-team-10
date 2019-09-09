16334db57abadefb12398c10e0c69020d2ff4c88
soufyakoub <splentercell.1997@gmail.com>
2019-09-09 13:46:12 +0100

    basic react router implementation
    couple points to make clear :
    
    -   the login form component  needs only a 'onSubmit' method that will be given the form 'data' and an 'invalidate' method .
    
        the invalidate method accepts a message that will be displayed on top of the form to simulate an error .
    
    -   the same thing applies for the Register form except that the 'invalidate' method accepts an object literal with property names the same as the id of the field(s) that will be getting the error message .
    
        the value of each property is the error message string .

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import configureAppStore from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
+
 import App from "./App";
 
+import "./index.scss";
+
 ReactDOM.render(
-	<Provider store={configureAppStore()}>
-		<App />
+	<Provider
+		store={configureAppStore({
+			user: null,
+			error: null // will be an object containing a message property , and a statusCode property
+		})}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById("root")
 );
