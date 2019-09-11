4aa563c118a184fabf18077551db60fb256cba76
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-09-11 10:26:08 +0100

    refactor user controller and added project controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,57 +1,75 @@
 const User = require('../models/user');
 
-const findUsers = async () => {
+const findUsers = async (res) => {
   try {
-    const users = await User.find();
-    return users;
+    const users = await User.find({});
+    if(users){
+      res.status(200).json(users);
+    }else{
+      res.status(400).json({error: 'Not Found'});
+    }
   } catch (error) {
     console.log(error);
-    return null;
+    res.status(500).json({error: 'Serveur Error'})
   }
 };
 
-const findUser = async id => {
+const findUser = async (id, res) => {
   try {
     const user = await User.findById(id);
-    return user;
+    if(user){
+      res.status(200).json(user)
+    }else{
+      res.status(404).json({error: 'Not Found'})
+    }
   } catch (error) {
     console.log(error);
-    return null;
+    res.status(500).json({error: 'Server Error'})
   }
 };
 
-const createUser = async ({ username, email, password }) => {
+const createUser = async (req, res) => {
+  const { username, email, password } = req.body;
   try {
     const user = await User.find({ username });
-    if (user) return { error: 'username exist' };
+    if (user) return res.status(400).json({error: 'Username already used'})
     const userEmail = await User.find({ email });
-    if (userEmail) return { error: 'email exist' };
+    if (userEmail) return res.status(400).json({error: 'Email already used'})
     const newUser = await User.create({ username, email, password });
     const savedUser = await newUser.save();
-    return savedUser;
+    return res.status(201).json(savedUser);
   } catch (error) {
     console.log(error);
-    return null;
+    res.status(500).json({error: 'Server Error'})
   }
 };
 
-const UpdateUser = async (id, update) => {
+const UpdateUser = async (id, req, res) => {
+  const update = req.body;
   try {
     const user = await User.findByIdAndUpdate({ id, update });
-    return user;
+    if(user){
+      res.status(200).json(user)
+    }else{
+      res.status(404).json({error: 'Not Found'})
+    }
   } catch (error) {
     console.log(error);
-    return null;
+    res.status(500).json({error: 'Server Error'})
   }
 };
 
-const deleteUser = async id => {
+const deleteUser = async (id, res) => {
   try {
     const user = await User.findByIdAndDelete(id);
-    return user;
+    if(user){
+      res.status(200).json(user)
+    }else{
+      res.status(404).json({error: 'Not Found'})
+    }
   } catch (error) {
     console.log(error);
-    return null;
+    res.status(500).json({error: 'Server Error'})
   }
 };
 
