60a81c103034fcb929336c30f5e0d80eec9201d8
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-09-11 17:32:13 +0100

    remove http-error modules

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 require("./db")
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
 const projectRouter = require('./routes/project')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,18 +28,12 @@ app.use('/project', projectRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).json({error: 'Route Not Found'})
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+ res.status(500).json({error: 'Internal Server Error'})
 });
 
 module.exports = app;
