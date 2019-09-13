dc42784e0d55011b2f44dcee69508e2d1a539ed7
jidemobell <jidd32@gmail.com>
2019-09-13 01:07:04 +0100

    add card styles and add card button

--- client/src/DashBoard.js
+++ client/src/DashBoard.js
@@ -3,6 +3,7 @@ import { Navbar, Nav, Form, Image } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Card from './components/Card/Card'
+import AddProject from './components/Card/AddProjectCard'
 
 export default function DashBoards(props) {
   return (
@@ -35,17 +36,27 @@ export default function DashBoards(props) {
         </div>
       </Navbar>
       <section className="dash-body flex-col-centered">
-        <div className="content">
+        <div className="content" data-aos="fade-in">
           {/* <div className="flex-row add">
             <a className="left" href='http://fakelink'>Add Project</a>
           </div> */}
-          <section className="projects-summary flex-col-centered">
+          <section className="projects-summary flex-col-centered"  data-aos="fade-up">
             <div>MY PROJECTS SUNNARY</div>
           </section>
-          <section className="projects-shelf flex-row">
+          <section className="projects-shelf flex-row"  data-aos="fade-up">
 						<Card />
 						<Card />
 						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+            <AddProject />
           </section>
         </div>
       </section>
