11a9aeed24081e0aa2a8093b2c039a512810687a
jidemobell <jidd32@gmail.com>
2019-09-14 14:30:11 +0100

    add and style new project form

--- client/src/container/Modals/AddProjectForm.js
+++ client/src/container/Modals/AddProjectForm.js
@@ -1,26 +1,42 @@
-import React, {useState} from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
-function AddProjectForm({showForm, handleCloseForm}) {
-	// const [show, setShow] = useState(false)
-	
-	// const handleShow = () => setShow(true);
-	// const handleClose = () => setShow(false)
+function AddProjectForm({ showForm, handleCloseForm }) {
+  // const [show, setShow] = useState(false)
+
+  // const handleShow = () => setShow(true);
+  // const handleClose = () => setShow(false)
   return (
     <>
-      <Modal show={showForm} onHide={handleCloseForm}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+      <Modal show={showForm} onHide={handleCloseForm} id="project-form">
+        <Modal.Header closeButton id="project-header">
+          <Modal.Title id="project-title">Start A Project</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseForm}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleCloseForm}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Form>
+          <Modal.Body>
+            <Form.Group controlId="projectName" >
+              <Form.Label className="form-labels">Project Name</Form.Label>
+              <Form.Control className="form-field" type="text" placeholder="Name Your Project" />
+            </Form.Group>
+            <Form.Group controlId="textArea" className="forms">
+              <Form.Label className="form-labels">Project Description</Form.Label>
+              <Form.Control
+								as="textarea"
+								className="form-field"
+                placeholder="Add a project description"
+                rows="3"
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseForm} id="close" className="hvr-shadow">
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleCloseForm} id="save" className="hvr-shadow">
+              Save
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   );
