eb25cb25a6d924133a9042c03dd29da7846077fd
jidemobell <jidd32@gmail.com>
2019-09-16 18:08:30 +0100

    update project board and add todo list ityems

---
+++ client/src/images/icons/icons.js
@@ -0,0 +1,27 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHome,
+  faSearch,
+  faTrash,
+  faPlusCircle,
+  faUsers,
+  faComments,
+  faSitemap,
+  faListUl,
+  faStickyNote,
+  faTasks
+} from "@fortawesome/free-solid-svg-icons";
+
+//create a library to hold icons and brands
+library.add(
+  faHome,
+  faSearch,
+  faTrash,
+  faPlusCircle,
+  faUsers,
+  faComments,
+  faSitemap,
+  faListUl,
+  faStickyNote,
+  faTasks
+);
