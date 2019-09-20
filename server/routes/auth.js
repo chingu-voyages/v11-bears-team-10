ee31211393d35a4b9761ad56fa3785df0796221c
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-09-20 16:02:27 +0100

    added v1 to all routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const auth = require('../controllers/auth');
 const User = require('../controllers/user')
 
-router.post('/login', (req, res) => {
+router.post('/v1/login', (req, res) => {
   auth(req, res);
 });
 
 
-router.post('/register', (req, res) => {
+router.post('/v1/register', (req, res) => {
   User.createUser(req, res);
 });
 
