a06d0a50dc5a5baf72c90a95b6a749ee024cd1aa
jidemobell <jidd32@gmail.com>
2019-09-21 14:14:46 +0100

    return deleted imports

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,15 @@ import { Container } from "react-bootstrap";
 
 import NavBar from "./navbar/NavBar";
 import MainContent from "./MainContent";
-import LandingPage from './components/LandingPage'
-// import Dashboard from './components/Dashboard'
-// import ProjectsBaord from './components/ProjectBoard/ProjectBoard'
+
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<NavBar />
-			{/* <Container as="main"> */}
-				<LandingPage />
-			{/* </Container> */}
+			<Container as="main">
+				<MainContent />
+			</Container>
 		</BrowserRouter>
 	);
 }
