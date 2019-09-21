dcbf4a69e6eca2058e8d70f04d7c7ad2ce967952
soufiane yakoubi <splentercell.1997@gmail.com>
2019-09-21 11:55:07 +0100

    implemented an input Validation class

--- client/src/redux/actionCreators.js
+++ client/src/redux/actionCreators.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Validation from "../validation";
 import { IS_STORAGE_AVAILABLE } from "../index";
 
 export const setUser = (user = null) => ({ type: "SET_USER", user });
@@ -12,60 +13,76 @@ export const setError = (statusCode = null, message = null, requestTimeout = fal
 
 export const resetError = () => ({ type: "RESET_ERROR" });
 
-export const register = (data, invalidate) => dispatch =>
-	axios
-		.post("/register", data)
-		.then(response => {
-			if (IS_STORAGE_AVAILABLE) {
-				localStorage.setItem("token", response.data.token);
-				localStorage.setItem("user_id", response.data.user._id);
-			}
-			dispatch(setUser(response.data.user));
-		})
-		.catch(e => {
-			if (e.response) {
-				const {
-					status,
-					data: { error }
-				} = e.response;
+export const register = (data, invalidate) => dispatch => {
+	const validation = new Validation(data, {
+		username: ["min:4", "max:20", "alphanumeric"],
+		password: ["min:8", "max:20", "password"],
+		email: "email"
+	});
+	validation.validate();
 
-				if (status === 401) {
-					const errors = {};
+	if (validation.passes)
+		axios
+			.post("/register", data)
+			.then(response => {
+				if (IS_STORAGE_AVAILABLE) {
+					localStorage.setItem("token", response.data.token);
+					localStorage.setItem("user_id", response.data.user._id);
+				}
+				dispatch(setUser(response.data.user));
+			})
+			.catch(e => {
+				if (e.response) {
+					const {
+						status,
+						data: { error }
+					} = e.response;
 
-					if (/username/i.test(error)) errors.username = error;
-					else if (/email/i.test(error)) errors.email = error;
-					else if (/password/i.test(error)) errors.password = error;
+					if (status === 401)
+						// for this status code the server returns an 'already used' message
+						// eslint-disable-next-line no-unused-vars
+						for (const regex of [/username/i, /email/i, /password/i]) {
+							let matches = error.match(regex);
+							if (matches) {
+								validation.addErrors("already used", matches[0].toLowerCase());
+								break;
+							}
+						}
+					else dispatch(setError(status));
+				} else dispatch(setError(null, null, e.code === "ECONNABORTED"));
 
-					return invalidate(errors);
-				} else dispatch(setError(status));
-			} else dispatch(setError(null, null, e.code === "ECONNABORTED"));
+				invalidate(validation.errors);
+			});
+	else invalidate(validation.errors);
+};
 
-			invalidate();
-		});
+export const login = (data, invalidate) => dispatch => {
+	const validation = new Validation(data, {
+		username: "min:1",
+		password: "min:1"
+	});
+	validation.validate();
 
-export const login = (data, invalidate) => dispatch =>
-	axios
-		.post("/login", data)
-		.then(response => {
-			if (IS_STORAGE_AVAILABLE) {
-				localStorage.setItem("token", response.data.token);
-				localStorage.setItem("user_id", response.data.user._id);
-			}
-			dispatch(setUser(response.data.user));
-		})
-		.catch(e => {
-			if (e.response) {
-				const {
-					status,
-					data: { error }
-				} = e.response;
+	if (validation.passes)
+		axios
+			.post("/login", data)
+			.then(response => {
+				if (IS_STORAGE_AVAILABLE) {
+					localStorage.setItem("token", response.data.token);
+					localStorage.setItem("user_id", response.data.user._id);
+				}
+				dispatch(setUser(response.data.user));
+			})
+			.catch(e => {
+				if (e.response) {
+					if (e.response.status === 401) return invalidate("invalid credentials");
+					else dispatch(setError(e.response.status));
+				} else dispatch(setError(null, null, e.code === "ECONNABORTED"));
 
-				if (status === 401 || status === 400) return invalidate(error);
-				else dispatch(setError(status));
-			} else dispatch(setError(null, null, e.code === "ECONNABORTED"));
-
-			invalidate();
-		});
+				invalidate();
+			});
+	else invalidate("username or password is empty");
+};
 
 export const logout = () => dispatch => {
 	if (IS_STORAGE_AVAILABLE) {
