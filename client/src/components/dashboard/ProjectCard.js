56aac85e0411afaa7c28a369c6f3b8572724995e
soufiane yakoubi <splentercell.1997@gmail.com>
2019-10-01 14:06:08 +0100

    sync front-end projectList with the updated api

--- client/src/components/dashboard/ProjectCard.js
+++ client/src/components/dashboard/ProjectCard.js
@@ -1,45 +1,35 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Row } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
 
-// function onClick(history, project_id) {
-// 	history.push("/project/" + project_id);
-// }
+function onClick(history, project) {
+	history.push("/project/" + project._id);
+}
 
-function ProjectCard({ project }) {
+function ProjectCard({ project, history }) {
 	return (
-		<Link to={`/project/${project._id}`}>
 		<div
-			className="project-card border border-secondary rounded p-3"
+			onClick={onClick.bind(null, history, project)}
+			className="project-card border border-secondary rounded bottom-shadow-on-hover p-3"
 			data-aos="fade-down">
-			<h2 className="text-center text-truncate mb-3">{project.title}</h2>
+			<h2 className="text-center text-truncate mb-4">{project.title}</h2>
 			<Row noGutters>
-				<Col xs={12} xl={6} className="text-center mb-3">
-					<small className="text-muted">
-						<span>created at : </span>
-						{project.date_create.substring(0, 10)}
-					</small>
-				</Col>
-				<Col xs={12} xl={6} className="text-right">
-					<i className="mr-4">
-						<FontAwesomeIcon icon="users" id="team-members" />
-						<span>{project.team.length}</span>
-					</i>
-					<i className="mr-4">
-						<FontAwesomeIcon icon="comments" id="messages" />
-						<span>{project.message_board.length}</span>
-					</i>
-					<i>
-						<FontAwesomeIcon icon="clipboard-list" id="todos" />
-						<span>{project.todos.length}</span>
-					</i>
-				</Col>
+				<i className="ml-auto mr-3">
+					<FontAwesomeIcon icon="users" id="team-members" />
+					<span>{project.nb_member}</span>
+				</i>
+				<i className="ml-0 mr-3">
+					<FontAwesomeIcon icon="comments" id="messages" />
+					<span>{project.nb_msg}</span>
+				</i>
+				<i className="ml-0 mr-3">
+					<FontAwesomeIcon icon="clipboard-list" id="todos" />
+					<span>{project.nb_todos}</span>
+				</i>
 			</Row>
 		</div>
-		</Link>
 	);
 }
 
-// export default withRouter(ProjectCard);
-export default ProjectCard;
+export default withRouter(ProjectCard);
--- client/src/components/dashboard/ProjectsList.js
+++ client/src/components/dashboard/ProjectsList.js
@@ -1,51 +1,34 @@
 import React from "react";
-import { Spinner, Row, Button, Col } from "react-bootstrap";
+import { Row, Button, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ProjectCard from "./ProjectCard";
 
-function renderProjectCards(projects) {
-	if (projects === null)
-		return (
-			<Spinner
-				animation="border"
-				variant="primary"
-				className="my-3 mx-auto big-spinner"
-				role="status">
-				<span className="sr-only">Fetching projects ...</span>
-			</Spinner>
-		);
-
-	if (!Array.isArray(projects))
-		return (
-			<p className="text-muted text-center mx-auto">
-				there was a problem loading your projects ! try refreching the page
-			</p>
-		);
-
-	if (!projects.length)
-		return (
-			<p className="text-muted text-center mx-auto">
-				you currently have no projects . start by creating one
-			</p>
-		);
-
-	return projects.map(project => (
-		<Col key={project._id} xs={12} sm={6} md={4} className="mb-3 p-2">
-			<ProjectCard project={project} />
-		</Col>
-	));
-}
-
-export default function ProjectsList({ projects }) {
+export default function ProjectsList({ projectList }) {
 	return (
-		<section id="project-list" className="p-3 mb-3 bg-white rounded" data-aos="fade-up">
+		<section
+			id="project-list"
+			className="p-3 mb-3 bg-white rounded bottom-shadow"
+			data-aos="fade-up">
 			<Row noGutters>
 				<h1 className="my-auto">Projects</h1>
-				<Button variant="dark-purple" className="ml-auto">
+				<Button as={Link} to="/haha" variant="dark-purple" className="ml-auto">
 					create a project
 				</Button>
 			</Row>
 			<hr className="w-100" />
-			<Row noGutters>{renderProjectCards(projects)}</Row>
+			<Row noGutters>
+				{projectList.length ? (
+					projectList.map(project => (
+						<Col key={project._id} xs={12} sm={6} md={4} className="mb-2 p-2">
+							<ProjectCard project={project} />
+						</Col>
+					))
+				) : (
+					<p className="text-muted text-center mx-auto">
+						you currently have no projects . start by creating one
+					</p>
+				)}
+			</Row>
 		</section>
 	);
 }
