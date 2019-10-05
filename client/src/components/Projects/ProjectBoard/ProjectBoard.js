129a544a2d3229fb0fe2e4978f7cccc54b4bfce6
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-05 06:57:50 +0100

    added fetchUsers datatlist

--- client/src/components/Projects/ProjectBoard/ProjectBoard.js
+++ client/src/components/Projects/ProjectBoard/ProjectBoard.js
@@ -2,23 +2,39 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { connect } from "react-redux";
 import { getProject } from "../../../redux/action_creators/project";
+import { fetshUsers } from '../../../redux/action_creators/usersListAction'
 import TodosBoard from "../../Todos/TodosBoard/TodosBoard";
 import MessageBoard from "../../Messages/MessageBoard";
 import AppPlaceholder from "../../../AppPlaceholder";
 
 function ProjectBoard(props) {
-  const { project, dispatch} = props;
+  const { project, usersList, dispatch} = props;
   const { id } = props.match.params;
-
+  
   const [showMessages, setShowMessages] = useState(false);
 	const [showTodos, setShowTodos] = useState(true);
-
+  const [users, setUsers] = useState(usersList);
+  const [team, setTeam] = useState([]);
+  
+  const handleChange = (e)=>{
+    const user = users.find(user => user._id === e.target.value)
+    console.log('user list before =', users)
+    setUsers(users.filter(user => user._id !== e.target.value))
+    console.log('user list after =', users)
+    setTeam([...team, user])
+    
+  }
+  
+  
   useEffect(() => {
     console.log('use effect getproject')
     if(id)
-      dispatch(getProject(id));
+    dispatch(getProject(id));
+    dispatch(fetshUsers())
+    setUsers(usersList)
   }, [id, dispatch]);
-
+  console.log('users =', users)
+  console.log('userlist=', usersList)
   return !project ? (
     <AppPlaceholder text="Loading..." />
   ) : (
@@ -26,10 +42,19 @@ function ProjectBoard(props) {
       <section className="board-body flex-col-centered">
         <div className="content" data-aos="fade-in">
           <section className="projects-summary flex-col" data-aos="fade-up">
-            <span>
+            <div>
               <h3>{project.title}</h3>
               <p>{project.description}</p>
-            </span>
+              <div>
+                {team.map(user => <span key={user._id}>{user.username}</span>)}
+              <input list="list-users" onChange={handleChange}/>
+              <datalist id="list-users" >
+                {users.map((user) => (
+                  <option key={user._id} value={user._id}>{user.username}</option>
+                ))}
+              </datalist>
+              </div>
+            </div>
             <hr />
             <section className="flex-row projects-items">
               <div className="flex-row">
@@ -37,11 +62,11 @@ function ProjectBoard(props) {
                   <FontAwesomeIcon icon="list-ul" />
                 </div>
                 <div
-                   className={`summary ${showTodos ? "clicked" : ""}`}
-                    onClick={() => {
-                      setShowMessages(false);
-                      setShowTodos(true);
-                    }}
+                  className={`summary ${showTodos ? "clicked" : ""}`}
+                  onClick={() => {
+                    setShowMessages(false);
+                    setShowTodos(true);
+                  }}
                 >
                   <h3>Todos</h3>
                 </div>
@@ -52,10 +77,10 @@ function ProjectBoard(props) {
                 </div>
                 <div
                   className={`summary ${showMessages ? "clicked" : ""}`}
-                    onClick={() => {
-                      setShowTodos(false);
-                      setShowMessages(true);
-                    }}
+                  onClick={() => {
+                    setShowTodos(false);
+                    setShowMessages(true);
+                  }}
                 >
                   <h3>Message Board</h3>
                 </div>
@@ -76,7 +101,8 @@ function ProjectBoard(props) {
 }
 const mapStatToProps = state => {
   return {
-    project: state.project
+    project: state.project,
+    usersList: state.usersList
   };
 };
 
