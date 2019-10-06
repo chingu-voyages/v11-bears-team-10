668be68174f926e992a0a97e3d063b797fdf0e91
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-06 17:50:07 +0100

    delete project from projectlist when user is removed form the project team

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -1,11 +1,10 @@
-
-const Project = require('../models/project');
-const User = require('../models/user');
+const Project = require("../models/project");
+const User = require("../models/user");
 
 const findUserProjects = async (userId, res) => {
   try {
     const projects = await Project.find({ admin: userId });
-    if (!projects.length) return res.status(404).json({ error: 'Not Found' });
+    if (!projects.length) return res.status(404).json({ error: "Not Found" });
     res.status(200).json({ projects });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -15,8 +14,8 @@ const findUserProjects = async (userId, res) => {
 const findProject = async (id, res) => {
   try {
     const project = await Project.findById(id);
-    if (!project) return res.status(404).json({ error: 'Not Found' });
-		res.status(200).json({ project });
+    if (!project) return res.status(404).json({ error: "Not Found" });
+    res.status(200).json({ project });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -24,19 +23,29 @@ const findProject = async (id, res) => {
 
 const createProject = async (userId, req, res) => {
   const project = req.body;
-  console.log('project =', project)
+  console.log("project =", project);
   try {
     const newProject = await Project.create({
       admin: userId,
       ...project
     });
     await newProject.save();
-    const user = await User.findByIdAndUpdate(userId, {
-      $push: {
-        projectList: { _id: newProject._id, title: newProject.title, nb_member: 1 }
-      }
-    }, { new: true });
-    res.status(201).json({ project: newProject, projectList: user.projectList.reverse() });
+    const user = await User.findByIdAndUpdate(
+      userId,
+      {
+        $push: {
+          projectList: {
+            _id: newProject._id,
+            title: newProject.title,
+            nb_member: 1
+          }
+        }
+      },
+      { new: true }
+    );
+    res
+      .status(201)
+      .json({ project: newProject, projectList: user.projectList.reverse() });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -45,56 +54,52 @@ const createProject = async (userId, req, res) => {
 const updateProject = async (id, req, res) => {
   const update = req.body;
   try {
+    const { team } = await Project.findById(id, "team");
     const project = await Project.findByIdAndUpdate(id, update, { new: true });
-    if (!project) return res.status(404).json({ error: 'Not Found' });
+    if (!project) return res.status(404).json({ error: "Not Found" });
     const adminId = project.admin;
-    const teamIds = project.team.map(user => user._id);
-    await updateUserProjectList(res, project, [adminId, ...teamIds]);
-    const user = await User.findById(adminId, 'projectList')
-    res.status(200).json({ project, projectList: user.projectList});
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const deleteProject = async (id, req, res) => {
-  console.log('---------delete project--------------')
-  try {
-    const project = await Project.findById(id);
-    if (!project) return res.status(404).json({ error: 'Not Found' });
-    
-    if (!project.admin.equals(req.user._id))
-    return res.status(401).send('UnAuthorized');
-    
-    await Project.findByIdAndDelete(id);
-    // const user = await User.findByIdAndUpdate(project.admin, {
-    //   $pull: { projectList: { _id: project._id } }
-    // }, {new: true});
-    const ids = project.team.map(user => user._id)
-    await User.updateMany({_id: {$in: ids}}, {
-      $pull: { projectList: { _id: project._id } }
-    }, {new: true});
-    const user = await User.findById(project.admin)
-    console.log('users delete project =', user)
-		
+    const teamToAddIds = project.team.map(user => user._id);
+    const teamToDeleteIds = team
+      .filter(
+        user =>
+          !project.team.find(el => el._id.toString() === user._id.toString())
+      )
+      .map(user => user._id);
+    await updateUserProjectList(
+      res,
+      project,
+      [adminId, ...teamToAddIds],
+      teamToDeleteIds
+    );
+    const user = await User.findById(adminId, "projectList");
     res.status(200).json({ project, projectList: user.projectList });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
-async function updateUserProjectList(res, project, users = []) {
+async function updateUserProjectList(
+  res,
+  project,
+  addPrjToUsers = [],
+  deleteProjectUsers
+) {
   const nb_todos = project.todos.length;
   const nb_msg = project.messages.length;
   const nb_member = project.team.length + 1;
   const completed = project.completed;
   await Promise.all(
-    users.map(async userId => {
+    addPrjToUsers.map(async userId => {
       try {
-        const user = await User.findById(userId,{password:0});
+        const user = await User.findById(userId, { password: 0 });
         const userPrj = user.projectList.id(project._id);
         if (userPrj) {
-          userPrj.set({ title: project.title, nb_todos, nb_msg, nb_member, completed });
+          userPrj.set({
+            title: project.title,
+            nb_todos,
+            nb_msg,
+            nb_member,
+            completed
+          });
         } else {
           user.projectList.push({
             _id: project._id,
@@ -105,13 +110,51 @@ async function updateUserProjectList(res, project, users = []) {
             completed
           });
         }
-       await user.save();
+        await user.save();
       } catch (error) {
         res.status(500).json({ error: error.message });
       }
     })
   );
+  try {
+    await User.updateMany(
+      { _id: { $in: deleteProjectUsers } },
+      {
+        $pull: { projectList: { _id: project._id } }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
+
+const deleteProject = async (id, req, res) => {
+  console.log("---------delete project--------------");
+  try {
+    const project = await Project.findById(id);
+    if (!project) return res.status(404).json({ error: "Not Found" });
+
+    if (!project.admin.equals(req.user._id))
+      return res.status(401).send("UnAuthorized");
+
+    await Project.findByIdAndDelete(id);
+    
+    const ids = project.team.map(user => user._id);
+    await User.updateMany(
+      { _id: { $in: ids } },
+      {
+        $pull: { projectList: { _id: project._id } }
+      },
+      { new: true }
+    );
+    const user = await User.findById(project.admin);
+    console.log("users delete project =", user);
+
+    res.status(200).json({ project, projectList: user.projectList });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
   findProject,
