9ae99a07ca37d5edadf1b2e85ed8f5cf9048c4db
soufiane yakoubi <splentercell.1997@gmail.com>
2019-10-08 22:07:00 +0100

    improved the error page styling

--- client/src/errors/ErrorPage.js
+++ client/src/errors/ErrorPage.js
@@ -3,8 +3,10 @@ import getErrorMessage from "./getErrorMessage";
 
 export default function ErrorPage({ error }) {
 	return (
-		<h1 id="error-page-message" className="m-auto p-4 text-light rounded-lg">
-			{getErrorMessage(error)}
-		</h1>
+		<div className="m-auto text-center text-light">
+			<h1 id="error-page-message" className="p-3 my-0 mx-2 rounded-lg">
+				{getErrorMessage(error)}
+			</h1>
+		</div>
 	);
 }
