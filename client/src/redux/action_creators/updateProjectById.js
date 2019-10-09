4662c2851912924edde7f9e92f1b6302849a6a02
soufiane yakoubi <splentercell.1997@gmail.com>
2019-10-10 00:32:33 +0100

    show success toast when the project is updated successfully

--- client/src/redux/action_creators/updateProjectById.js
+++ client/src/redux/action_creators/updateProjectById.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import setToastError from "./setToastError";
+import setToastSuccess from "./setToastSuccess";
 
 const updateProjectById = (project, callback, error_callback) => dispatch =>
 	axios
 		.put(`project/${project._id}`, project)
-		.then(r => callback(r.data.project))
+		.then(r => {
+			dispatch(setToastSuccess('updated successfully'))
+			callback(r.data.project)
+		})
 		.catch(e => {
 			dispatch(setToastError(e));
 			error_callback();
