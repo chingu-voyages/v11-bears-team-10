c09fa968f7d9aeac40bbb60a4f55a33a7184c2b1
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-10 19:48:18 +0100

    added counter action

--- client/src/redux/action_creators/chatAction.js
+++ client/src/redux/action_creators/chatAction.js
@@ -2,36 +2,48 @@ import openSocket from "socket.io-client";
 
 const socket = openSocket(`127.0.0.1:8000`);
 
-export default function configureSocketIo(){
-  console.log('-------------config socket io--------------')
-  return (dispatch, getstate) =>{
-    const {projectList} = getstate().user
-    console.log('config socket projeclist =', projectList)
+export default function configureSocketIo() {
+  return (dispatch, getstate) => {
+    const { projectList } = getstate().user;
     socket.on("users", function(userList) {
-      console.log("recieves users list")
-      dispatch({type: "SET_CHAT_USERS", userList})
+      dispatch({ type: "SET_CHAT_USERS", userList });
     });
 
     projectList.forEach(prj => {
       socket.on(prj.title, function(msg) {
-        console.log("---------------------------------------------------------------------------------------------------------------------recieves messages")
-        console.log('msg =', msg)
-        const messages = getstate().chat.messages || {}
-      let update = [msg];
-      if (messages[prj.title]) {
-        update = [...messages[prj.title], msg];
-      }
-        dispatch({type: "UPDATE_MESSAGES_LIST", messages: { ...messages, [prj.title]: update }})        
+      console.log("message recieved =", msg)
+        const messages = getstate().chat.messages || {};
+        const newMessagesCounter = getstate().chat.newMessagesCounter || {}
+        if (!messages[prj.title]) {
+          messages[prj.title] = [];
+        }
+        if(messages[prj.title].find(message => message._id === msg._id)) return
+        const update = [...messages[prj.title], msg];
+        const counter = newMessagesCounter[prj.title] ?  newMessagesCounter[prj.title] + 1 : 1;
+        dispatch({
+          type: "UPDATE_MESSAGES_LIST",
+          messages: { ...messages, [prj.title]: update }
+        });
+        dispatch({
+          type: "UPDATE_MESSAGES_COUNTER",
+          updateCounter: { [prj.title]: counter }
+        });
       });
     });
 
     const projectTitles = projectList.map(prj => prj.title);
     socket.emit("create", projectTitles);
     socket.emit("login", getstate().user.username);
-
-  }
+  };
 }
-export function sendMessage({chatRoom, username, message}){
-  console.log({chatRoom, username, message})
-    socket.emit(chatRoom, { username, message });
+export async function sendMessage({ chatRoom, username, message }) {
+  await socket.emit(chatRoom, { username, message });
+}
+export function resetCounter(prj){
+  return dispatch =>{
+    dispatch({
+      type: "UPDATE_MESSAGES_COUNTER",
+      updateCounter: { [prj]: 0 }
+    });
+  }
 }
