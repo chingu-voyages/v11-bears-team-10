22e9e832bad9be65dccc5988ec0dbd3f9a6b689f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-10 19:46:40 +0100

    fix bug save messages an userlist

--- client/src/redux/reducers/chatReducer.js
+++ client/src/redux/reducers/chatReducer.js
@@ -1,15 +1,14 @@
-const chat = (state = {}, action)=>{
+const chat = (state = {}, action) => {
   switch (action.type) {
     case "UPDATE_MESSAGES_LIST":
-      return {...state, messages:{...action.messages}}  
+      return { ...state, messages: { ...action.messages } };
     case "SET_CHAT_USERS":
-        return {...state,  userList:{...action.userList}}  
+      return { ...state, userList: [...action.userList] };
+    case "UPDATE_MESSAGES_COUNTER":
+      return { ...state, newMessagesCounter: {...state.newMessagesCounter, ...action.updateCounter } };
     default:
       return state;
   }
-}
+};
 
 export default chat;
-
-
-
