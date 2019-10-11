0f3608055e1c731caa3c4312e9080f4339795fd2
soufiane yakoubi <splentercell.1997@gmail.com>
2019-10-12 00:34:03 +0100

    fix: MessageToast always has a background primary

--- client/src/reusable_components/MessageToast.js
+++ client/src/reusable_components/MessageToast.js
@@ -31,10 +31,11 @@ export default class MessageToast extends Component {
 
 	render() {
 		var bg = "primary";
-		var className = `p-3 bg-${bg} text-light text-center m-2 fixed-bottom-right`;
 
 		if (this.props.error && !this.props.success) bg = "danger";
 		if (!this.props.error && this.props.success) bg = "success";
+
+		var className = `p-3 bg-${bg} text-light text-center m-2 fixed-bottom-right`;
 		if (this.props.className) className += " " + this.props.className;
 
 		return (
