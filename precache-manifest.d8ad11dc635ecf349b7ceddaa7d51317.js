053aa9499e4475ac70ed23303854bbf7d956c9e4
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-10-12 22:43:44 +0100

    Updates

---
+++ precache-manifest.d8ad11dc635ecf349b7ceddaa7d51317.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2031187476b3fa8f29c33f5cab4ed2",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/index.html"
+  },
+  {
+    "revision": "27b489c94e5f2cd81d06",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/css/2.ffea287f.chunk.css"
+  },
+  {
+    "revision": "f0da54cdb71a66520e45",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/css/main.68dcc85d.chunk.css"
+  },
+  {
+    "revision": "27b489c94e5f2cd81d06",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/js/2.6e6903ed.chunk.js"
+  },
+  {
+    "revision": "f0da54cdb71a66520e45",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/js/main.f2dcf0b4.chunk.js"
+  },
+  {
+    "revision": "bb38abc572f6d0a936cc",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/js/runtime~main.9d9a373e.js"
+  },
+  {
+    "revision": "45b4e205fc065c8522f9d3a686b97459",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/media/complete.45b4e205.png"
+  },
+  {
+    "revision": "60ee9dbc27580484d873844f24ee42b0",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/media/landing.60ee9dbc.png"
+  },
+  {
+    "revision": "8ce2cdf1db036d3880df451ee21e6d5f",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/media/message.8ce2cdf1.png"
+  },
+  {
+    "revision": "88a2df489b4cc7a2ee75ebc367c21f9f",
+    "url": "/chingu-voyages.github.io/v11-bears-team-10/static/media/options.88a2df48.png"
+  }
+]);
